563039a7d7c3e722242b67e512d92d3d8eae9eed
SeSa99 <sesa99@stud.uni-obuda.hu>
2025-01-06 22:22:30 +0100

    Added task module, task's crud components and routing to them.

---
+++ frontend/TrelloCopy-frontend/src/app/tasks/tasks.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TasksRoutingModule } from './tasks-routing.module';
+import { TasksComponent } from './tasks.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskEditComponent } from './components/task-edit/task-edit.component';
+import { TaskCreateComponent } from './components/task-create/task-create.component';
+
+
+@NgModule({
+  declarations: [
+    TasksComponent,
+    TaskListComponent,
+    TaskEditComponent,
+    TaskCreateComponent
+  ],
+  imports: [
+    CommonModule,
+    TasksRoutingModule
+  ]
+})
+export class TasksModule { }
