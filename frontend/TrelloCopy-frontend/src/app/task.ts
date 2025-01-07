73ea5e6b897ca2ec34ab165b46a7ebcf62b79151
SeSa99 <sesa99@stud.uni-obuda.hu>
2025-01-07 20:42:55 +0100

    Added task class, task service

--- frontend/TrelloCopy-frontend/src/app/task.ts
+++ frontend/TrelloCopy-frontend/src/app/task.ts
@@ -1,2 +1,9 @@
 export class Task {
+    id: number | null = null
+    name: string = ''
+    description: string = ''
+    isCompleted: boolean | null = null
+    hasOwner: boolean | null = null
+    //Owner
+    projectId: number | null = null
 }
